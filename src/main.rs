a8ea5c782cfe82cca049f07baa75ada65fda8ba8
ishanpandey <ishan.pandey@infra.market>
2023-12-02 21:40:09 +0530

    add basic boilerplate

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+
+use std::io;
+use std::io::{Write, BufRead, BufReader, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Hello, world!");
+}
