8fb3ea608d13595010467605fbdbc7359b2739ba
ishanpandey <ishan.pandey@infra.market>
2023-12-02 21:54:28 +0530

    add .

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # learn-rust
+
+##### main
+    - fn name(<args>) {<body>}
+    - macro is with !
+    - mutable - mut -> value can be changed, else it cannot be changed
+    - string formatting
+    - error handling in rust
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,15 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).expect("didn't receive input"); // &mut -> directly saves value entered to name; read_line() returns enum
+    // enum has fixed number of possible values, ok / error; error handling is built into rust using expect()
+    // handling this error can be done too...
+
+    let greeting: &str = "Nice to meet you!!";
+
+    println!("Hello! {}!, {}", name.trim_end(), greeting); // {} is used to pass values directly; trim_end() removes newline char from end of name string
+
+
 }
